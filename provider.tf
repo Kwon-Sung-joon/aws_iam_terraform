57524910cdb1f8dd9458bea9180e6cc89f79173d
ksj <kofdx7@gmail.com>
2024-03-05 15:53:23 +0900

    add tf

--- provider.tf
+++ provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 5.3"
     }
   }
+  backend "s3" {
+    bucket  = "ksj-tf-state"
+    key     = "KSJ_TERRAFORM_IAM/terraform.tfstate"
+    region  = "ap-northeast-2"
+    encrypt = true
+  }
 }
 
 provider "aws" {
