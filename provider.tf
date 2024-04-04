1dcd457098ea6c599e8c1e9fbe1ff2018182df0e
ksj <kofdx7@gmail.com>
2024-04-04 16:06:37 +0900

    edit backend

--- provider.tf
+++ provider.tf
@@ -6,8 +6,8 @@ terraform {
     }
   }
   backend "s3" {
-    bucket  = "ts-infra-team-tfstate"
-    key     = "ts1439/terraform.tfstate"
+    bucket  = "<your-bucket>"
+    key     = "terraform.tfstate"
     region  = "ap-northeast-2"
     encrypt = true
   }
