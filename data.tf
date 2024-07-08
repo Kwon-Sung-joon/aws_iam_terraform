90937c93a044737425ef39db70b3bc61514264c8
SungJoon Kwon <43159901+Kwon-Sung-joon@users.noreply.github.com>
2024-07-08 16:27:55 +0900

    Update data.tf

--- data.tf
+++ data.tf
@@ -3,7 +3,7 @@ data "aws_iam_policy_document" "ec2_scheduler_role" {
   statement {
     actions = ["sts:AssumeRole"]
     principals {
-      identifiers = ["arn:aws:iam::589566835476:role/AWS_IAM_ROLE_CROSS_ACCOUNT_SCHEDULER"]
+      identifiers = ["arn:aws:iam::<ACCOUNT_A>:role/AWS_IAM_ROLE_CROSS_ACCOUNT_SCHEDULER"]
       type        = "AWS"
     }
   }
@@ -14,8 +14,7 @@ data "aws_iam_policy_document" "assume_role_for_read_only" {
   statement {
     actions = ["sts:AssumeRole"]
     principals {
-      identifiers = ["arn:aws:iam::589566835476:role/IAM_ROLE_FOR_LAMBDA"]
-
+      identifiers = ["arn:aws:iam::<ACCOUNT_A>:role/IAM_ROLE_FOR_LAMBDA"]
       type        = "AWS"
     }
   }
