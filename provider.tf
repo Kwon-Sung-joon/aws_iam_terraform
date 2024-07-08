127746680318623fa6c8617c2cece86be09afcfe
SungJoon Kwon <43159901+Kwon-Sung-joon@users.noreply.github.com>
2024-07-08 16:12:52 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -16,7 +16,7 @@ terraform {
 provider "aws" {
   region = "ap-northeast-2"
   assume_role {
-    role_arn     = "arn:aws:iam::589566835476:role/IAM_ROLE_FOR_ASSUME_ROLE"
+    role_arn     = "arn:aws:iam::<ACCOUNT-A>:role/IAM_ROLE_FOR_ASSUME_ROLE"
     session_name = "hompage"
   }
 }
@@ -25,79 +25,7 @@ provider "aws" {
   region = "ap-northeast-2"
   alias = "air"
   assume_role {
-    role_arn     = "arn:aws:iam::751732153713:role/IAM_ROLE_FOR_ASSUME_ROLE"
+    role_arn     = "arn:aws:iam::<ACCOUNT-B>:role/IAM_ROLE_FOR_ASSUME_ROLE"
     session_name = "air"
-  }
-}
-#hot
-provider "aws" {
-  alias  = "hot"
-  region = "ap-northeast-2"
-  assume_role {
-    role_arn     = "arn:aws:iam::298385711329:role/IAM_ROLE_FOR_ASSUME_ROLE"
-    session_name = "hot"
-  }
-}
-#comm
-provider "aws" {
-  alias  = "comm"
-  region = "ap-northeast-2"
-  assume_role {
-    role_arn     = "arn:aws:iam::300846112004:role/IAM_ROLE_FOR_ASSUME_ROLE"
-    session_name = "comm"
-  }
-}
-#tna_dev
-provider "aws" {
-  alias  = "tna_dev"
-  region = "ap-northeast-2"
-  assume_role {
-    role_arn     = "arn:aws:iam::012788139911:role/IAM_ROLE_FOR_ASSUME_ROLE"
-    session_name = "tna_dev"
-  }
-}
-#ndc
-provider "aws" {
-  alias  = "ndc"
-  region = "ap-northeast-2"
-  assume_role {
-    role_arn     = "arn:aws:iam::744690697308:role/IAM_ROLE_FOR_ASSUME_ROLE"
-    session_name = "ndc"
-  }
-}
-#dw
-provider "aws" {
-  alias  = "dw"
-  region = "ap-northeast-2"
-  assume_role {
-    role_arn     = "arn:aws:iam::792931648816:role/IAM_ROLE_FOR_ASSUME_ROLE"
-    session_name = "dw"
-  }
-}
-#tna
-provider "aws" {
-  alias  = "tna"
-  region = "ap-northeast-2"
-  assume_role {
-    role_arn     = "arn:aws:iam::339927058960:role/IAM_ROLE_FOR_ASSUME_ROLE"
-    session_name = "tna"
-  }
-}
-#viet
-provider "aws" {
-  alias  = "viet"
-  region = "ap-northeast-2"
-  assume_role {
-    role_arn     = "arn:aws:iam::836881754257:role/IAM_ROLE_FOR_ASSUME_ROLE"
-    session_name = "viet"
-  }
-}
-#dms
-provider "aws" {
-  alias  = "dms"
-  region = "ap-northeast-2"
-  assume_role {
-    role_arn     = "arn:aws:iam::097345172411:role/IAM_ROLE_FOR_ASSUME_ROLE"
-    session_name = "dms"
   }
 }
